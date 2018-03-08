e75e4727d3c67149db525eb359fdd1a71873f286
evertonmessias <everton.messias@gmail.com>
2018-03-07 23:04:07 -0300

    07-03-2018_23:04:07

---
+++ java01/src/java01/Java01.java
@@ -0,0 +1,11 @@
+
+package java01;
+
+public class Java01 {
+
+    public static void main(String[] args) { // psvm
+        // TODO code application logic here
+        System.out.println("Ola Mundo"); // sout
+    }
+    
+}
