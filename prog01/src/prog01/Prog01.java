8520ce3c3564c166224cf1b3d7440253478d99af
evertonmessias <everton.messias@gmail.com>
2018-03-07 22:44:27 -0300

    07-03-2018_22:44:15

---
+++ prog01/src/prog01/Prog01.java
@@ -0,0 +1,12 @@
+
+package prog01;
+
+
+public class Prog01 {
+
+    public static void main(String[] args) { // psbm
+        System.out.print("Ola Mundo\n");
+        System.out.println("Primeiro Prog JAVA"); // sout
+    }
+    
+}
