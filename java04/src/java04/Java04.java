9ecdb7dd10e65dc0e20d4dd915d70097b0b4319c
everton <everton@messias>
2018-03-09 16:52:31 -0300

    09-03-2018_16:52:00

--- java04/src/java04/Java04.java
+++ java04/src/java04/Java04.java
@@ -2,30 +2,37 @@
 package java04;
 import java.util.Scanner;
 
-
 public class Java04 {
 
-
 private String nome;
-private double nota1,nota2,media;
+private float nota1,nota2;
 
-public double calcular(){
-    return (this.nota1 + this.nota2)/2;       
-}
-
-
+public float media(){return (this.nota1 + this.nota2)/2;}
 
 public static void main(String[]args){
+String matricula = "12345"; 
+boolean casado = true;
+char sexo = 'M';
+int idade = 30;
+
+String casadoo = Boolean.toString(casado);
+int mat = Integer.parseInt(matricula);
 Scanner ler= new Scanner(System.in);
 Java04 aluno= new Java04();
 System.out.println("Digite seu nome:");
 aluno.nome=ler.nextLine();
 System.out.println("Digite sua nota 1:");
-aluno.nota1=ler.nextDouble();
+aluno.nota1=ler.nextFloat();
 System.out.println("Digite sua nota 2:");
-aluno.nota2=ler.nextDouble();
-aluno.media = aluno.calcular();
-System.out.printf("\nAluno: %s\nMédia: %.2f\n\n",aluno.nome,aluno.media);
-}
-}
-    
+aluno.nota2=ler.nextFloat();
+//
+System.out.printf("\nDados:"
+        + "------------\n"
+        + "Nome: %s\n"
+        + "Matric: %d\n"
+        + "Sexo: %c , Idade: %d\n"
+        + "Casado: %s\n"
+        + "Média: %.2f\n"
+        + "------------\n"
+        + "\n",aluno.nome,mat,sexo,idade,casado,aluno.media());
+}}
