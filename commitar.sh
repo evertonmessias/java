75091da6cb5b907599122b23173b3254d2e78940
everton <everton@messias>
2018-03-12 14:58:55 -0300

    12-03-2018_14:58:55

--- commitar.sh
+++ commitar.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+clear
 rm .gitignore
-git add .
+echo "*********** REMOVIDO GITIGNORE OK ******************"
+git add *.*
+echo "**************** GIT ADD OK ************************"
 data=`date +%d-%m-%Y_%H:%M:%S`
 git commit -m $data
+echo "*************** GIT COMMIT OK **********************"
 git push
