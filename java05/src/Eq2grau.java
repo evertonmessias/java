7348ee732ecbf51a5d6340bcb5501e2f8176e506
everton <everton@messias>
2018-03-12 14:56:41 -0300

    12-03-2018_14:56:00

---
+++ java05/src/Eq2grau.java
@@ -0,0 +1,49 @@
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+
+public class Eq2grau {
+    
+    public float valorA, valorB, valorC, d, x1, x2, vc[] = new float[4];
+    public char q = (char)178;
+    
+    /*this não é usado pois as modificações dos atributos vem de fora da Classe
+    (this é usado para modificar atributos dentro da propria classe, refere-se ao objeto que chamou ...)
+    */
+        
+    public String escrever(){        
+        String eq, a, b, c;
+        a = String.format("%.1f",valorA);
+        b = String.format("%.1f",valorB);
+        c = String.format("%.1f",valorC);
+        if(valorB < 0 && valorC < 0){
+        eq = a+"x"+q+" "+b+"x "+c+" = 0";}
+        else if(valorB >= 0 && valorC < 0){
+        eq = a+"x"+q+" +"+b+"x "+c+" = 0";}
+        else if(valorB < 0 && valorC >= 0){
+        eq = a+"x"+q+" "+b+"x +"+c+" = 0";}
+        else{
+        eq = a+"x"+q+" +"+b+"x +"+c+" = 0";}
+        return eq; }
+      
+    public float[] calcular(){        
+        if(valorA == 0) {vc[0] = 0;vc[1] = 0;vc[2] = 0;vc[3] = 0;}// vc[3] == 0 => "A Não pode ser Zero!"
+        else {d=(float) ((pow(valorB, 2))-(4*valorA*valorC)); // cálculo do delta               
+        if (d < 0){vc[0] = d;vc[1] = 0;vc[2] = 0;vc[3] = 1;}// vc[3] == 1 => "Não Existe Raizes Reais!"
+        else{x1=(float) (((-1*valorB)+(sqrt(d)))/(2*valorA)); // cálculo de Basckara
+             x2=(float) (((-1*valorB)-(sqrt(d)))/(2*valorA));
+             vc[0] = d;vc[1] = x1;vc[2] = x2;vc[3] = 2;}} // vc[3] == 2 => ok
+        return vc;}
+    
+    public String resposta(int op){
+        switch (op) {
+            case 0:
+                return "Valor 'A' não pode ser Zero!";
+            case 1:
+                return "Não Existe Raizes Reais!";
+            default:
+                return escrever();
+        }}   
+    
+}
--- java05/src/Java05.java
+++ java05/src/Java05.java
@@ -1,46 +1,7 @@
 
-import static java.lang.Math.pow;
-import static java.lang.Math.sqrt;
-
 public class Java05 extends javax.swing.JFrame {
     
-    float valorA, valorB, valorC, d, x1, x2, vc[] = new float[4];
-    int op;char q = (char)178;
-        
-    String escrever(){        
-        String eq, a, b, c;
-        a = String.format("%.1f",valorA);
-        b = String.format("%.1f",valorB);
-        c = String.format("%.1f",valorC);
-        if(valorB < 0 && valorC < 0){
-        eq = a+"x"+q+" "+b+"x "+c+" = 0";}
-        else if(valorB >= 0 && valorC < 0){
-        eq = a+"x"+q+" +"+b+"x "+c+" = 0";}
-        else if(valorB < 0 && valorC >= 0){
-        eq = a+"x"+q+" "+b+"x +"+c+" = 0";}
-        else{
-        eq = a+"x"+q+" +"+b+"x +"+c+" = 0";}
-        return eq; }
-      
-    float[] calcular(){        
-        if(valorA == 0) {vc[0] = 0;vc[1] = 0;vc[2] = 0;vc[3] = 0;}// vc[3] == 0 => "A Não pode ser Zero!"
-        else {d=(float) ((pow(valorB, 2))-(4*valorA*valorC)); // cálculo do delta               
-        if (d < 0){vc[0] = d;vc[1] = 0;vc[2] = 0;vc[3] = 1;}// vc[3] == 1 => "Não Existe Raizes Reais!"
-        else{x1=(float) (((-1*valorB)+(sqrt(d)))/(2*valorA)); // cálculo de Basckara
-             x2=(float) (((-1*valorB)-(sqrt(d)))/(2*valorA));
-             vc[0] = d;vc[1] = x1;vc[2] = x2;vc[3] = 2;}} // vc[3] == 2 => ok
-        return vc;}
-    
-    String resposta(int op){
-        switch (op) {
-            case 0:
-                return "Valor 'A' não pode ser Zero!";
-            case 1:
-                return "Não Existe Raizes Reais!";
-            default:
-                return escrever();
-        }
-    }
+    char q = (char)178; // quadrado   
     
     public Java05() {
         initComponents();
@@ -201,19 +162,24 @@ public class Java05 extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    // BOTÕES:   
+    
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
        String a = valA.getText();String b = valB.getText();String c = valC.getText();
        if ("".equals(a) || "".equals(b) || "".equals(c) || !isNumeric(a) || !isNumeric(b) || !isNumeric(c)){
        resposta.setText("Digite os Valores Numéricos!");}
        else{
-       valorA = Float.parseFloat(a);
-       valorB = Float.parseFloat(b);
-       valorC = Float.parseFloat(c); 
-       op = (int)calcular()[3];
-       resposta.setText(resposta(op));
-       String de = String.format("%.2f",calcular()[0]);
-       String r1 = String.format("%.2f",calcular()[1]);
-       String r2 = String.format("%.2f",calcular()[2]);
+           
+       Eq2grau equac = new Eq2grau();
+       
+       equac.valorA = Float.parseFloat(a);
+       equac.valorB = Float.parseFloat(b);
+       equac.valorC = Float.parseFloat(c); 
+       int opc = (int)equac.calcular()[3];
+       resposta.setText(equac.resposta(opc));
+       String de = String.format("%.2f",equac.calcular()[0]);
+       String r1 = String.format("%.2f",equac.calcular()[1]);
+       String r2 = String.format("%.2f",equac.calcular()[2]);
        delta.setText("Delta = "+de);
        raiz1.setText("X1 = "+r1);
        raiz2.setText("X2 = "+r2);
@@ -234,6 +200,9 @@ public class Java05 extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+        
+        // método principal não usado !
+        
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -289,6 +258,7 @@ public class Java05 extends javax.swing.JFrame {
 		} catch (NumberFormatException ex) {
 			return false;
 		}
-		return true;
-	}
+		return true;}
+   
+   
 }
